0abc4d87003a412e051e54587edf7819b0f653ad
Олег Волков <olegdizzvolkov@yandex.ru>
2023-02-01 16:47:11 +0300

    add STM32F407VGT6

---
+++ CMSIS/stm32f407VGT6/Clean_project/Core/Inc/main.h
@@ -0,0 +1,15 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stm32f4xx.h>
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ CMSIS/stm32f407VGT6/script.ps1
@@ -0,0 +1,10 @@
+$name = $args[0]
+(Get-Content .cproject) | ForEach-Object { $_ -replace "Clean_project", $name } | Set-Content .cproject
+(Get-Content .mxproject) | ForEach-Object { $_ -replace "Clean_project", $name } | Set-Content .mxproject
+(Get-Content .project) | ForEach-Object { $_ -replace "Clean_project", $name } | Set-Content .project
+(Get-Content Clean_project.ioc) | ForEach-Object { $_ -replace "Clean_project", $name } | Set-Content Clean_project.ioc
+(Get-Content STM32F407VGTX_FLASH.ld) | ForEach-Object { $_ -replace "Clean_project", $name } | Set-Content STM32F407VGTX_FLASH.ld
+(Get-Content STM32F407VGTX_RAM.ld) | ForEach-Object { $_ -replace "Clean_project", $name } | Set-Content STM32F407VGTX_RAM.ld
+$filename = $name + ".ioc"
+Rename-Item Clean_project.ioc $filename
+echo "Job is done"
