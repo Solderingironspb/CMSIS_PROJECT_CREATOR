a75072b39c47a6af6ab31282f92515a8e7e65ede
Solderingironspb <68805120+Solderingironspb@users.noreply.github.com>
2024-12-23 23:23:59 +0300

    add STM32F030F4P6

---
+++ CMSIS/stm32f030F4P6/Clean_project/Debug/Core/Src/system_stm32f0xx.d
@@ -0,0 +1,19 @@
+Core/Src/system_stm32f0xx.o: ../Core/Src/system_stm32f0xx.c \
+ ../Drivers/CMSIS/stm32f0xx.h ../Drivers/CMSIS/stm32f030x6.h \
+ ../Drivers/CMSIS/core_cm0.h ../Drivers/CMSIS/cmsis_version.h \
+ ../Drivers/CMSIS/cmsis_compiler.h ../Drivers/CMSIS/cmsis_gcc.h \
+ ../Drivers/CMSIS/system_stm32f0xx.h
+
+../Drivers/CMSIS/stm32f0xx.h:
+
+../Drivers/CMSIS/stm32f030x6.h:
+
+../Drivers/CMSIS/core_cm0.h:
+
+../Drivers/CMSIS/cmsis_version.h:
+
+../Drivers/CMSIS/cmsis_compiler.h:
+
+../Drivers/CMSIS/cmsis_gcc.h:
+
+../Drivers/CMSIS/system_stm32f0xx.h:
